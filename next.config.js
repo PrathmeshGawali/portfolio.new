7c3e0f12d1457ca21793272c631406406ad31b4c
PrathmeshGawali <prathmeshg@gmail.com>
2025-07-04 18:25:23 +0530

    Deployment changes

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: 'export', // Enables static site generation
+  images: {
+    unoptimized: true, // Disables image optimization for static export
+  },
+  experimental: {
+    serverComponentsExternalPackages: ['stream'], // Fixes stream error
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
